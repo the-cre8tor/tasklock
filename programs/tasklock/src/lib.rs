012f4dc3768f82ca584cf0157fc8d749ef37911b
Alexander Nitiola <anitiola@aellacredit.com>
2025-01-06 15:48:30 +0100

    first commit

---
+++ programs/tasklock/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("BzsbqDh3B1zyXVDWQWBTBYLgN5LDRxShxLmAutUZtMfT");
+
+#[program]
+pub mod tasklock {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
